1eea10f6d9fafc80772c828a41d56ab8d37279fe
sz-xcf <3071696354@qq.com>
2018-10-18 09:11:31 +0800

    mask numpy arr

--- maskPic.py
+++ maskPic.py
@@ -11,3 +11,12 @@ img[:, :, 1][mask] = 255
 img[:, :, 2][mask] = 255
 
 im.imsave('pics/dotpic_masked.jpg', img)
+
+
+# we kan mask any numpy arr, for e.g.
+# x_train.shape = (10000, 28, 28, 1)
+# mask0 = (x_train[0][..., 0] < 0.5)
+# mask1 = (1-mask0).astype(np.bool)
+# print(mask0)
+# print(mask1)
+
