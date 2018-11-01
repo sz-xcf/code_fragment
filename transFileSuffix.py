3d3329538acba6d3ed39af3baf33b9ff14be8408
sz-xcf <3071696354@qq.com>
2018-11-01 15:53:09 +0800

    refactoring transFileSuffix

--- transFileSuffix.py
+++ transFileSuffix.py
@@ -1,82 +1,91 @@
 
+# This script is used for transfering the file suffix, to avoiding encryption
+# of files with those suffix in certain environment, where the cp command can
+# not work for this purpose
+
+# e.g.
+# python transFileSuffix.py --src_dir `pwd`/somedir_txt --dst_dir `pwd`/somedir 
+# --src_suffix .pytxt --dst_suffix .py --recursive --take_action #--ignore_other
+
 import sys
 import os
 import shutil
+import argparse
 
-def take_op_on_dir(path, operate, *operate_args, recursive_flag=False):
+parser = argparse.ArgumentParser()
+parser.add_argument("--src_dir", type=str, default='', help='the dir which you want to copy from')
+parser.add_argument("--dst_dir", type=str, default='', help='the dir which you want copy to')
+parser.add_argument("--src_suffix", type=str, default='', help='the suffix you want modify from while copying')
+parser.add_argument("--dst_suffix", type=str, default='', help='the suffix you want modify to while copying')
+parser.add_argument("--take_action", action='store_true', default=False, help='take action, or else only print action')
+parser.add_argument("--ignore_other", action='store_true', default=False, help='ignore files not end with the target suffix')
+parser.add_argument("--recursive", action='store_true', default=False, help='take action for the dir recursively')
+
+opt = parser.parse_args()
+print(opt)
+
+def take_op_on_dir(path, operate, *operate_args, recursive=False):
     """take operation on target dir, recursively or not"""
     parents = os.listdir(path)
     for parent in parents:
         child = os.path.join(path, parent)
         if os.path.isdir(child):
-            if recursive_flag:
-                take_op_on_dir(child, operate, *operate_args, recursive_flag=recursive_flag)
-            else:
-                pass
+            if recursive:
+                take_op_on_dir(child, operate, *operate_args, recursive=recursive)
+
         else:
             operate(child, *operate_args)
 
-def show_usage():
-    print('This script is used for transfering the file suffix, to avoiding cryption')
-    print('of files with those suffix in certain environment, where the cp command can')
-    print('not work\n')
 
-    print('usage: python transFileFuffix.py [dir_name] -decode/-encode {-r}\n')
-
-    #TODO: use sth likewise parsearg to improve this.
-
-def cp_and_change_suffix(src_filename, src_dir, dst_dir, src_suffix, dst_suffix):
+def cp_and_change_suffix(src_filename, src_dir, dst_dir, src_suffix, dst_suffix, ignore_other, take_action=False):
     """
-    e.g. cp_and_change_suffix('aaa/bbb/ccc/ddd.py', 'ccc', 'zzz'), then get a file
-    named 'aaa/bbb/zzz/ddd.pytxt'
-    until now, use the abs path, otherwise it may break down
+    this operation is dangerous, you'd better print the action first, (set take_action=False)
+    and then make sure if you want to take actions.
+
+    until now, use the abs path, relative path has not been tested.
     """
 
-    if src_dir == dst_dir:
-        # if same, will it cause a infinite loop? not sure.
-        print('Err: dst_dir must be different from src_dir')
-        return
+    dst_path = os.path.dirname(src_filename)
+    dst_path = dst_path.replace(src_dir, dst_dir, 1)
+
+    if take_action and (not os.path.exists(dst_path)):
+        os.makedirs(dst_path)
 
     src_tuple = os.path.splitext(src_filename)
-    dst_path = src_tuple[0].replace(src_dir, dst_dir, 1)
+    dst_filename = src_tuple[0].replace(src_dir, dst_dir, 1)
 
-    if os.path.splitext(src_filename)[1] == src_suffix:
-        dst_filename = dst_path + dst_suffix
+    if src_tuple[1] == src_suffix:
+        dst_filename = dst_filename + dst_suffix
+        print('cp: ', src_filename, ' ====> ', dst_filename)
+        if take_action:
+            # print('take action now')
+            shutil.copyfile(src_filename, dst_filename)
+
     else:
-        if not os.path.exists(dst_path):
-            os.makedirs(dst_path)
+        if ignore_other:
+            return
 
-        # I hope this more fast than 'cat' or 'cp'
-        # dst_filename = dst_path + src_tuple[1]
-        dst_filename = dst_path + dst_suffix
-        print('cp: ', src_filename, '--->', dst_filename)
-        shutil.copyfile(src_filename, dst_filename)
+        dst_filename = dst_filename + src_tuple[1]
+        # if take op on other file
+        print('cp: ', src_filename, ' ----> ', dst_filename)
+        if take_action:
+            # print('take action now')
+            shutil.copyfile(src_filename, dst_filename)
+
+
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3 or len(sys.argv) > 4:
-        show_usage()
+
+    if not os.path.exists(opt.src_dir):
+        print('Err: path not exist: ', opt.src_dir)
         sys.exit()
 
-    if not os.path.exists(sys.argv[1]):
-        print('Path not exist: ', sys.argv[1])
+    if opt.src_dir == opt.dst_dir:
+        # if same, will it cause a infinite loop? not sure.
+        print('Err: dst_dir must be different from src_dir')
         sys.exit()
 
-    if ((sys.argv[2] != '-decode') and (sys.argv[2] != '-encode')):
-        show_usage()
-        sys.exit()
+    take_op_on_dir(opt.src_dir, cp_and_change_suffix, opt.src_dir, opt.dst_dir, 
+            opt.src_suffix, opt.dst_suffix, opt.ignore_other, opt.take_action, recursive = opt.recursive)
 
-    recursive = False
-    if (len(sys.argv) == 4):
-            if (sys.argv[3] != '-r'):
-                show_usage()
-                sys.exit()
-            else:
-                recursive = True
-
-    # take_op_on_path(sys.argv[1], print, recursive_flag=recursive)
-    # I need some argparse tool, ...
-    # take_op_on_dir(sys.argv[1], cp_and_change_suffix, 
-            # 'code_fragment', 'TTTTEST', '.py', '.pytxt', recursive_flag=recursive)
-    take_op_on_dir(sys.argv[1], cp_and_change_suffix, 
-            'code_fragment', 'TTTTEST', '.py', '.pytxt', recursive_flag=recursive)
 
